6998899a2840a315793e752acde515b666d15f70
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-06-02 15:15:40 +0200

    test(server): check for rejected cors header

--- test/acceptance/server.js
+++ test/acceptance/server.js
@@ -7,4 +7,14 @@ describe('GET /', () => {
   it('returns 404 for index', done => {
     app.get('/').expect(404, done);
   });
+
+  it('reject invalid cors domain', done => {
+    app.get('/')
+      .set('Origin', 'https://invalid.com')
+      .expect(403)
+      .expect({
+        code: 403,
+        message: 'Bad Origin "https://invalid.com"',
+      }, done);
+  });
 });

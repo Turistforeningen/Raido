550e81159ee037736f9100eedf3f2dfae479a0b6
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-06-11 00:03:32 +0200

    refactor: make separate file for health integration check

---
+++ test/integration/health.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const assert = require('assert');
+const request = require('supertest');
+const app = request(require('../../'));
+
+describe('GET /CloudHealthCheck', () => {
+  it('returns PostgreSQL status', done => {
+    app.get('/CloudHealthCheck')
+      .set('Origin', 'https://example2.com')
+      .expect(200)
+      .expect(res => {
+        assert.equal(res.body.code, 200);
+        assert.equal(res.body.message, 'Ok');
+        assert.equal(res.body.services.length, 1);
+        assert.equal(res.body.services[0].name, 'PostgreSQL');
+        assert.equal(typeof res.body.services[0].status, 'object');
+      })
+      .end(done);
+  });
+});
--- test/integration/server.js
+++ test/integration/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const assert = require('assert');
 const request = require('supertest');
 const app = request(require('../../'));
 
@@ -17,21 +16,5 @@ describe('GET /', () => {
         code: 403,
         message: 'Bad Origin "https://invalid.com"',
       }, done);
-  });
-});
-
-describe('GET /CloudHealthCheck', () => {
-  it('returns PostgreSQL status', done => {
-    app.get('/CloudHealthCheck')
-      .set('Origin', 'https://example2.com')
-      .expect(200)
-      .expect(res => {
-        assert.equal(res.body.code, 200);
-        assert.equal(res.body.message, 'Ok');
-        assert.equal(res.body.services.length, 1);
-        assert.equal(res.body.services[0].name, 'PostgreSQL');
-        assert.equal(typeof res.body.services[0].status, 'object');
-      })
-      .end(done);
   });
 });

aca514b52b203af125305d3ec15996e49f18111f
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-06-13 00:35:07 +0200

    feat(routing): separate coords param into source and target
    BRRAKING CHANGE: Support for the old `coords` parameter has been
    dropped. Use `source` and `target` parameters instead.

--- controllers/api.js
+++ controllers/api.js
@@ -7,12 +7,15 @@ const app = express();
 const HttpError = require('@starefossen/http-error');
 
 app.get('/routing', (req, res, next) => {
-  if (!req.query.coords || req.query.coords.split(',').length !== 4) {
-    return next(new HttpError('Missing or invalid "coords" query', 400));
+  if (!req.query.source || req.query.source.split(',').length !== 2
+    || !req.query.target || req.query.target.split(',').length !== 2
+  ) {
+    return next(new HttpError('Missing or invalid coordinates', 400));
   }
 
   // Make sure all the coords are float values
-  const coords = req.query.coords.split(',').map(c => parseFloat(c, 10));
+  const source = req.query.source.split(',').map(c => parseFloat(c, 10));
+  const target = req.query.target.split(',').map(c => parseFloat(c, 10));
 
   const path_buffer = Math.min(parseInt(req.query.path_buffer || 2000, 10), 4000);
   const point_buffer = Math.min(parseInt(req.query.point_buffer || 10, 10), 100);
@@ -20,7 +23,8 @@ app.get('/routing', (req, res, next) => {
   const sql = `
     SELECT ST_AsGeoJSON(ST_Transform(geom, 4326)) as geojson, cost
     FROM path(
-      ${coords.join(',')},
+      ${source.join(',')},
+      ${target.join(',')},
       path_buffer:=${path_buffer},
       point_buffer:=${point_buffer}
     )
@@ -31,10 +35,7 @@ app.get('/routing', (req, res, next) => {
     if (err) { return next(new HttpError('Database Query Failed', 500, err)); }
 
     if (!result.rows.length || !result.rows[0].geojson) {
-      return res.json({ type: 'LineString', coordinates: [
-        [coords[0], coords[1]],
-        [coords[2], coords[3]],
-      ] });
+      return res.json({ type: 'LineString', coordinates: [source, target]});
     }
 
     const geojson = {
